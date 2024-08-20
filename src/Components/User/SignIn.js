abff9a1cacc701b5bb13f7f22c35efe65ca85068
kmvishnu <kmvishnu1996@gmail.com>
2024-08-21 01:35:53 +0530

    Added a helptext for login

--- src/Components/User/SignIn.js
+++ src/Components/User/SignIn.js
@@ -1,29 +1,35 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useUser } from '../../Hooks/useUser';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useUser } from "../../Hooks/useUser";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Snackbar, Alert } from "@mui/material";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://kmvishnu.github.io/angular-app">
         Webworms
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -31,18 +37,19 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isEmailValid, setIsEmailValid] = useState(false);
   const [isPasswordValid, setIsPasswordValid] = useState(false);
   const [isFormValid, setIsFormValid] = useState(false);
   const { loading, error, loginUser } = useUser();
+  const [snackbarOpen, setSnackbarOpen] = useState(true);
   const navigate = useNavigate();
   const token = useSelector((state) => state.user.token);
 
   useEffect(() => {
     if (token) {
-      navigate('/home', { replace: true });
+      navigate("/home", { replace: true });
     }
   }, [token, navigate]);
 
@@ -68,22 +75,22 @@ export default function SignIn() {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const userData = {
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     };
     try {
       const response = await loginUser(userData);
-      if (response.status === 'success') {
-        sessionStorage.setItem('isAuthenticated', 'true');
-        navigate('/home', { replace: true });
+      if (response.status === "success") {
+        sessionStorage.setItem("isAuthenticated", "true");
+        navigate("/home", { replace: true });
       }
     } catch (error) {
-      console.error('Error logging in:', error);
+      console.error("Error logging in:", error);
     }
   };
 
   const handleNavigateToRegister = () => {
-    navigate('/register');
+    navigate("/register");
   };
 
   return (
@@ -93,18 +100,23 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -117,7 +129,9 @@ export default function SignIn() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               error={!isEmailValid && email.length > 0}
-              helperText={!isEmailValid && email.length > 0 ? 'Invalid email format' : ''}
+              helperText={
+                !isEmailValid && email.length > 0 ? "Invalid email format" : ""
+              }
             />
             <TextField
               margin="normal"
@@ -131,7 +145,11 @@ export default function SignIn() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               error={!isPasswordValid && password.length > 0}
-              helperText={!isPasswordValid && password.length > 0 ? 'Password must be between 4 to 20 characters' : ''}
+              helperText={
+                !isPasswordValid && password.length > 0
+                  ? "Password must be between 4 to 20 characters"
+                  : ""
+              }
             />
             <Button
               type="submit"
@@ -140,7 +158,7 @@ export default function SignIn() {
               sx={{ mt: 3, mb: 2 }}
               disabled={!isFormValid || loading}
             >
-              {loading ? 'Loading...' : 'Sign In'}
+              {loading ? "Loading..." : "Sign In"}
             </Button>
             {error && (
               <Typography color="error" sx={{ mt: 2 }}>
@@ -152,7 +170,7 @@ export default function SignIn() {
                 <Typography
                   variant="body2"
                   color="primary"
-                  sx={{ cursor: 'pointer' }}
+                  sx={{ cursor: "pointer" }}
                   onClick={handleNavigateToRegister}
                 >
                   {"Don't have an account? Sign Up"}
@@ -162,6 +180,22 @@ export default function SignIn() {
           </Box>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
+
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={7000}
+          onClose={() => setSnackbarOpen(false)}
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }} // Set Snackbar position
+          sx={{ marginBottom: "150px" }} // Adjust bottom margin to position above the Add button
+        >
+          <Alert
+            onClose={() => setSnackbarOpen(false)}
+            severity="info"
+            sx={{ width: "100%" }}
+          >
+            Use email: "test@gmail.com", password : "test" for testing
+          </Alert>
+        </Snackbar>
       </Container>
     </ThemeProvider>
   );
