672f04a7d6b07a8d7abf34bef0017b8f5fc3e833
Vishnu K M <87776465+kmvishnu@users.noreply.github.com>
2024-12-18 13:51:03 +0000

    Server URL updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     
-    apiBaseUrl:'https://today-backend-production.up.railway.app'
-  };
-  
-  export default config;
+  apiBaseUrl:'https://today-backend-4ilg.onrender.com'
+};
+
+export default config;
